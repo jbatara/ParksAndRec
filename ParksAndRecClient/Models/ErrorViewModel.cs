b937bac605c1f398a4b035df0d1d72e846f06a11
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-11-01 08:25:53 -0700

    Initialize files for ParksAndRec Client side

---
+++ ParksAndRecClient/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ParksAndRecClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
