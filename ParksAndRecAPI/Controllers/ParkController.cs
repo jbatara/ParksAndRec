61fcb86cb38e97767d8291bebdb97aa599e5fc0f
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-11-01 14:58:32 -0700

    Add department and park controllers

--- ParksAndRecAPI/Controllers/ParkController.cs
+++ ParksAndRecAPI/Controllers/ParkController.cs
@@ -3,40 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ParksAndRecAPI.Models;
 
 namespace ParksAndRecAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class ParkController : ControllerBase
     {
-        // GET api/values
+        private ParksAndRecDbContext _db;
+
+        public ParkController(ParksAndRecDbContext db)
+        {
+            _db = db;
+        }
+
+        // GET api/park
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
+            var query = _db.Words.AsQueryable();
+            var returnObject = new Object(){};
+            returnObject.Parks = query.ToList();
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET api/park/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST api/park
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT api/park/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/park/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
