b2eb8b067850962d04aae5e73786ac71f55af6b0
Student <example@example.com>
2019-11-01 11:16:51 -0700

    Add db context for api

---
+++ ParksAndRecAPI/Models/ParksAndRecContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ParksAndRecAPI.Models {
+    public class ParksAndRecContext : DbContext {
+        public ParksAndRecContext (DbContextOptions<ParksAndRecContext> options) : base (options) { }
+
+        public DbSet<Park> Parks { get; set; }
+        // protected override void OnModelCreating (ModelBuilder builder) {
+        //     builder.Entity<Park> ()
+        //         .HasData (
+        //             new Word { WordId = 1, Name = "moist", Rating = 2, RatingCount = 1 },
+        //             new Word { WordId = 2, Name = "damp", Rating = 1, RatingCount = 1 },
+        //             new Word { WordId = 3, Name = "hug", Rating = 4, RatingCount = 1 },
+        //             new Word { WordId = 4, Name = "burnt", Rating = 5, RatingCount = 1 },
+        //             new Word { WordId = 5, Name = "vomit", Rating = 1, RatingCount = 1 }
+        //         );
+        // }
+
+    }
+}
