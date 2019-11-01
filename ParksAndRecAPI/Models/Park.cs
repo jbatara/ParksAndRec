3b0ad46c9c7f2903c4fabf390e1634511f611f04
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-11-01 13:36:55 -0700

    Add required annotations for certain properties of the department and park objects

--- ParksAndRecAPI/Models/Park.cs
+++ ParksAndRecAPI/Models/Park.cs
@@ -1,18 +1,21 @@
-namespace ParksAndRecAPI.Models
-{
-    public class Park
-    {
-        public int ParkId {get;set;}
-        public string Name {get;set;}
-        public string Address {get;set;}
-        public string City {get;set;}
-        public string State {get;set;}
-        public int ZipCode {get;set;}
+using System.ComponentModel.DataAnnotations;
+namespace ParksAndRecAPI.Models {
+    public class Park {
+        public int ParkId { get; set; }
 
-        public double Acres {get;set;}
-        public int YearFounded {get;set;}
-        public int DepartmentId {get;set;}
+        [Required]
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
 
-        public Department Department {get;set;}
+        public double Acres { get; set; }
+        [Range(1872,2019, ErrorMessage="The year founded must be after or equal to The Yellowstone Act of 1872")]
+        public int YearFounded { get; set; }
+        [Required]
+        public int DepartmentId { get; set; }
+
+        public Department Department { get; set; }
     }
 }
