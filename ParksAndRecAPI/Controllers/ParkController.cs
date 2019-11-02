3f067a8e7fcd74c89170b65cc207a3bc022ee12f
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-11-01 20:19:55 -0700

    Add methods for get, post, put, and delete for parks route

--- ParksAndRecAPI/Controllers/ParkController.cs
+++ ParksAndRecAPI/Controllers/ParkController.cs
@@ -3,54 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ParksAndRecAPI.Models;
 
-namespace ParksAndRecAPI.Controllers
-{
-    [Route("api/[controller]")]
+namespace ParksAndRecAPI.Controllers {
+    [Route ("api/[controller]")]
     [ApiController]
-    public class ParkController : ControllerBase
-    {
+    public class ParkController : ControllerBase {
         private ParksAndRecDbContext _db;
 
-        public ParkController(ParksAndRecDbContext db)
-        {
+        public ParkController (ParksAndRecDbContext db) {
             _db = db;
         }
 
         // GET api/park
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
-        {
-            var query = _db.Words.AsQueryable();
-            var returnObject = new Object(){};
-            returnObject.Parks = query.ToList();
-            return new string[] { "value1", "value2" };
+        public ActionResult<IEnumerable<Park>> Get (int parkId, int departmentId, string parkList) {
+            var query = _db.Parks.AsQueryable ();
+            if (parkId != 0) {
+                query.Where (p => p.ParkId == parkId);
+            }
+            if (departmentId != 0) {
+                query.Where (p => p.DepartmentId == departmentId);
+            }
+            if (parkList != null) {
+                parkList = parkList.ToLower ();
+                string[] parkArray = parkList.Split (",");
+                query = query.Where (p => parkArray.Contains (p.Name));
+            }
+            return query.ToList ();
         }
 
         // GET api/park/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
+        [HttpGet ("{id}")]
+        public ActionResult<string> Get (int id) {
+            var query = _db.Parks.AsQueryable ();
+            return query.FirstOrDefault (p => p.ParkId == id).ToString ();
         }
 
         // POST api/park
         [HttpPost]
-        public void Post([FromBody] string value)
-        {
+        public void Post ([FromBody] Park newPark) {
+            _db.Add (newPark);
+            _db.SaveChanges ();
         }
 
         // PUT api/park/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
+        [HttpPut ("{id}")]
+        public void Put (int id, [FromBody] Park editPark) {
+            _db.Entry (editPark).State = EntityState.Modified;
+            _db.SaveChanges();
         }
 
         // DELETE api/park/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        [HttpDelete ("{id}")]
+        public void Delete (int id) 
         {
-        }
+            var parkToDelete = _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
+            _db.Parks.Remove(parkToDelete);
+            _db.SaveChanges();
+         }
     }
 }
