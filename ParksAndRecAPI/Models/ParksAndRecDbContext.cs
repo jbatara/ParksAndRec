8ea3c2f27031c730c8f5ea343ed64e4d769709d9
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-11-03 20:13:12 -0800

    Change DepartmentApiKey to ApiKey class

--- ParksAndRecAPI/Models/ParksAndRecDbContext.cs
+++ ParksAndRecAPI/Models/ParksAndRecDbContext.cs
@@ -9,7 +9,7 @@ namespace ParksAndRecAPI.Models
     public DbSet<Park> Parks { get; set; }
     public DbSet<Department> Departments { get; set; }
 
-    public DbSet<DepartmentApiKey> DepartmentApiKeys { get; set; }
+    public DbSet<ApiKey> ApiKeys { get; set; }
     protected override void OnModelCreating(ModelBuilder builder)
     {
       builder.Entity<Park>()
@@ -17,14 +17,14 @@ namespace ParksAndRecAPI.Models
               new Park { ParkId = 1, Name = "Pwnee Commons", YearFounded = 1986, DepartmentId = 2 },
               new Park { ParkId = 2, Name = "Harvey James Park", DepartmentId = 2 },
               new Park { ParkId = 3, Name = "Lafayette Park", DepartmentId = 2 },
-              new Park { ParkId = 3, Name = "Eagleton Park", DepartmentId = 3 },
-              new Park { ParkId = 4, Name = "Maplewood Park", DepartmentId = 2 },
-              new Park { ParkId = 5, Name = "North Pawnee Recreation Center", DepartmentId = 2 },
-              new Park { ParkId = 6, Name = "Wamapokestone Park", DepartmentId = 2 },
-              new Park { ParkId = 7, Name = "Tucker Park", DepartmentId = 2 },
-              new Park { ParkId = 8, Name = "Pawnee Park: Smallest Park in Indiana", DepartmentId = 2 },
-              new Park { ParkId = 9, Name = "Slippery Elm Park", DepartmentId = 2 },
-              new Park { ParkId = 10, Name = "Ramsett Park", DepartmentId = 2 }
+              new Park { ParkId = 4, Name = "Eagleton Park", DepartmentId = 3 },
+              new Park { ParkId = 5, Name = "Maplewood Park", DepartmentId = 2 },
+              new Park { ParkId = 6, Name = "North Pawnee Recreation Center", DepartmentId = 2 },
+              new Park { ParkId = 7, Name = "Wamapokestone Park", DepartmentId = 2 },
+              new Park { ParkId = 8, Name = "Tucker Park", DepartmentId = 2 },
+              new Park { ParkId = 9, Name = "Pawnee Park: Smallest Park in Indiana", DepartmentId = 2 },
+              new Park { ParkId = 10, Name = "Slippery Elm Park", DepartmentId = 2 },
+              new Park { ParkId = 11, Name = "Ramsett Park", DepartmentId = 2 }
           );
       builder.Entity<Department>()
           .HasData(
