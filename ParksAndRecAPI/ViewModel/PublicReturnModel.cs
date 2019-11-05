90c05c20e936e2336cf89dc66baf989afe5c8dcd
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-11-04 21:52:41 -0800

    Add PublicReturnModel to have restricted view model for api return

--- ParksAndRecAPI/ViewModel/PublicReturnModel.cs
+++ ParksAndRecAPI/ViewModel/PublicReturnModel.cs
@@ -4,7 +4,7 @@ namespace ParksAndRecAPI.ViewModels
 {
   public class PublicReturnModel
   {
-    public string Park { get; set; }
+    public string Name { get; set; }
     public string Address { get; set; }
     public string City { get; set; }
     public string State { get; set; }
