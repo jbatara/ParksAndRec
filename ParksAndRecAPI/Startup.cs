b9ce449a7237a2eb70eb5dc8537b09380b8254e0
Jennifer Batara <jennifer.batara.dev@gmail.com>
2019-11-05 07:12:54 -0800

    Add Swagger UI to API

--- ParksAndRecAPI/Startup.cs
+++ ParksAndRecAPI/Startup.cs
@@ -12,33 +12,52 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using ParksAndRecAPI.Models;
+using Microsoft.OpenApi.Models;
 
-namespace ParksAndRecAPI {
-    public class Startup {
-        public Startup (IConfiguration configuration) {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices (IServiceCollection services) {
-            services.AddDbContext<ParksAndRecDbContext> (opt =>
-                opt.UseMySql (Configuration.GetConnectionString ("DefaultConnection")));
-            services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_2_2);
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
-            if (env.IsDevelopment ()) {
-                app.UseDeveloperExceptionPage ();
-            } else {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts ();
-            }
-
-            // app.UseHttpsRedirection ();
-            app.UseMvc ();
-        }
+namespace ParksAndRecAPI
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<ParksAndRecDbContext>(opt =>
+         opt.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+      services.AddSwaggerGen(c =>
+          {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "ParksNRecApi", Version = "v1" });
+          });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      app.UseSwagger();
+      app.UseSwaggerUI(c =>
+          {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "ParksNRecApi V1");
+            c.RoutePrefix=string.Empty;
+          });
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+      }
+
+      // app.UseHttpsRedirection ();
+      app.UseMvc();
+    }
+  }
 }
